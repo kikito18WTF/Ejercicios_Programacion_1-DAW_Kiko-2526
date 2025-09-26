5d033ac0dccd4e924b8fd08852698c4667468910
A615\1DAW <kikoperezcanto10@gmail.com>
2025-09-26 17:18:52 +0200

    Ejerciicos bateria 1 2 3

---
+++ Ejercicios_Programacion_Kiko/src/main/java/org/example/Ejercicio3.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+    static void main(){
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Dime un numero: ");
+
+        int num= entrada.nextInt();
+
+        if (num>0){
+
+            System.out.println("El "+ num + " es positivo");
+        } else if (num<0) {
+            System.out.println("El "+ num + " es negativo");
+        }else {
+            System.out.println("El " +num+ " es natural");
+        }
+
+
+    }
+}
