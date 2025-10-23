c019c9c0658026c388dc478615e2d4b1294ad3c4
A615\1DAW <kikoperezcanto10@gmail.com>
2025-10-23 17:21:31 +0200

    Ejercicios Bucles

---
+++ Ejercicios_Programacion_Kiko/src/main/java/org/example/Bucles.java
@@ -0,0 +1,118 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Bucles {
+
+    static void main() {
+
+        Scanner entrada = new Scanner(System.in);
+
+//        String contra = "admin1234";
+//
+//
+//
+//        do {
+//            System.out.println("Introduce una contraseña");
+//            String respuesta = entrada.next();
+//
+//            if (respuesta.equals(contra) ){
+//                break;
+//            }
+//            System.out.println("Has entrado Imbecil");
+//        }while (true);
+//
+//        while (true){
+//
+//            System.out.println("Inserta una palabra(o Salir):");
+//            String palabra = entrada.next();
+//
+//            if (palabra.toLowerCase().equals("salir")){
+//                System.out.println("Saliendo...");
+//                break;
+//            }
+//
+//            if (palabra.equals("Anulado")){
+//                System.out.println("palabra prohibida");
+//                continue;
+//            }
+//
+//            System.out.println(palabra + "GOL");
+//        }
+
+//        for (int i=1; i<50; i++){
+//
+//
+//            if (i % 3 == 0){
+//                continue;
+//            }
+//            System.out.println(i);
+//
+//        }
+//
+//        System.out.println("Introduce un numero: ");
+//        int num = entrada.nextInt();
+//        for (int i = num; i>0; i--){
+//            System.out.println(i);
+//        }
+//
+//        System.out.println("Introduce una palabra");
+//        String palabra = entrada.next();
+//
+//        int tamanyo = palabra.length();
+//
+//        //palabra para adelante
+//        for (int i = 0; i < tamanyo; i++){
+//            System.out.println(palabra.charAt(i));
+//        }
+//
+//        //palabra para atras
+//        for (int i = tamanyo -1; i >= 0; i--){
+//            System.out.println(palabra.charAt(i));
+//        }
+//
+//        //palabra para que salga en la misma linea
+//        for (int i = tamanyo -1; i >= 0; i--){
+//            System.out.print(palabra.charAt(i));
+//        }
+
+//        System.out.println("TABLA DE MULTIPLICAR");
+//        System.out.println("Introduce un numero para multiplicar");
+//        int num = entrada.nextInt();
+//
+//        for (int i = 0; i<= 10 ; i++ ){
+//
+//            int resultado = num * i;
+//            System.out.println(num +" x "+ i+  "= " + resultado);
+//
+//        }
+
+//        int suma =0;
+//        System.out.println("Cuantos numero quieres sumar:");
+//        int num = entrada.nextInt();
+//
+//        for (int i=0; i<num; i++){
+//            System.out.println("Inserta un numero");
+//            int num2 = entrada.nextInt();
+//
+//            suma+=num2;
+//
+//        }
+//
+//        System.out.println("El resultado es " + suma);
+
+
+        System.out.println("Introduce un numero");
+        int num = entrada.nextInt();
+
+        for (int i=1; i<num ; i++ ){
+
+        }
+
+
+
+
+
+
+    }
+}
